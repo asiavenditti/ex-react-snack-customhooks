2f261e8182da088e8416fc414d87133a0fd23980
Your Name <you@example.com>
2025-11-05 12:28:20 +0100

    complete milestone 2: create custom hook useDate to return current date and update every second

---
+++ src/useDate.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+export default function useDate() {
+
+    const [currentDate, setCurrentDate] = useState(new Date())
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentDate(new Date())
+        }, 1000);
+
+        return () => clearInterval(intervalId)
+
+    }, [])
+
+    return currentDate
+
+}
